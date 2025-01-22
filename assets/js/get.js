3b7e544599478f5d954ee61e894562c7e6c2274f
Zhikang-Rong <116642864+Zhikang-Rong@users.noreply.github.com>
2025-01-22 13:54:44 +0100

    Add files via upload

--- assets/js/get.js
+++ assets/js/get.js
@@ -50,42 +50,50 @@ function fetchJSONData() {
                 document.getElementById("juegos").innerHTML += content;
             }
             else if(document.getElementById("juego") !== null){
-                let found = false;
-                for (const game of data) {
-                    if(found == true){
-                        break;
-                    }
-                    if(game.id == urlParams.get("gameId")){
-                        found = true;
-                        document.title = "Tienda - "+game.name;
-                        content += "<img src='assets/img/"+game.image+"' alt='"+game.name+"'>";
-                        content += "<div>";
-                        content += "<h2 tabindex='0'>"+game.name+"</h2>";
-                        content += "<table>";
-                        content += "<tr>";
-                        content += "<td>Desarrollador</td>";
-                        content += "<td>"+game.developer+"</td>";
-                        content += "</tr>";
-                        content += "<tr>";
-                        content += "<td>Lanzado el</td>";
-                        content += "<td>"+game.launch+"</td>";
-                        content += "</tr>";
-                        content += "</table>";
-                        content += "<p tabindex='0'>"+game.description+"</p>";
-                        content += "</div>";
-                        content += "<div>";
-                        if(game.price == 0){
-                            content +="<span class='price' tabindex='0'>Gratis</span>";
-                        }else{
-                            content +="<span class='price' tabindex='0'>"+game.price+"€</span>";
+                if(urlParams.has("gameId")){
+                    let found = false;
+                    for (const game of data) {
+                        if(found == true){
+                            break;
                         }
-                        content += "<button tabindex='0' id='buy' aria-labelledby='price buy'>Comprar</button>";
-                        content += "</div>";
-                        document.querySelector('meta[property="og:image"]').setAttribute("content", "assets/img/"+game.image);
-                        document.querySelector('meta[property="og:title"]').setAttribute("content", "Tienda - "+game.name);
+                        if(game.id == urlParams.get("gameId")){
+                            found = true;
+                            document.title = "Tienda - "+game.name;
+                            content += "<img src='assets/img/"+game.image+"' alt='"+game.name+"'>";
+                            content += "<div>";
+                            content += "<h2 tabindex='0'>"+game.name+"</h2>";
+                            content += "<table>";
+                            content += "<tr>";
+                            content += "<td>Desarrollador</td>";
+                            content += "<td>"+game.developer+"</td>";
+                            content += "</tr>";
+                            content += "<tr>";
+                            content += "<td>Lanzado el</td>";
+                            content += "<td>"+game.launch+"</td>";
+                            content += "</tr>";
+                            content += "</table>";
+                            content += "<p tabindex='0'>"+game.description+"</p>";
+                            content += "</div>";
+                            content += "<div>";
+                            if(game.price == 0){
+                                content +="<span class='price' tabindex='0'>Gratis</span>";
+                            }else{
+                                content +="<span class='price' tabindex='0'>"+game.price+"€</span>";
+                            }
+                            content += "<button tabindex='0' id='buy' aria-labelledby='price buy'>Comprar</button>";
+                            content += "</div>";
+                            document.querySelector('meta[property="og:image"]').setAttribute("content", "assets/img/"+game.image);
+                            document.querySelector('meta[property="og:title"]').setAttribute("content", "Tienda - "+game.name);
+                        }
                     }
+                    if(found == true){
+                        document.getElementById("juego").innerHTML += content;
+                    }else{
+                        window.location.replace("https://zhikang-rong.github.io/");
+                    }
+                }else{
+                    window.location.replace("https://zhikang-rong.github.io/");
                 }
-                document.getElementById("juego").innerHTML += content;
             }
             
         })
